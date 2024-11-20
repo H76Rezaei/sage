cfedff62165074f765e5a4bef4d191e9df59e0f7
SarahBeH <sarrabenhamdene01@gmail.com>
2024-11-20 16:30:42 +0100

    modified requirements.text

--- chatbot-backend/main.py
+++ chatbot-backend/main.py
@@ -7,7 +7,7 @@ import json
 import torch
 import asyncio
 
-
+#checking my gpu
 print(torch.cuda.is_available())  # Should return True
 print(torch.cuda.get_device_name(0))  # Should show your GPU name
 
