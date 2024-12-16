51cfaa15eebd4d02fb3487ee81c473e83b546013
SarahBeH <sarrabenhamdene01@gmail.com>
2024-12-16 20:10:39 +0100

    speechApi.js

--- chatbot-frontend/src/services/speechApi.js
+++ chatbot-frontend/src/services/speechApi.js
@@ -1,5 +1,9 @@
 export async function streamAudioFromBackend(audioBlob) {
-  const url = "http://127.0.0.1:8000/conversation-audio-stream";
+  //streaming
+  //const url = "http://127.0.0.1:8000/conversation-audio-stream";
+
+  //no streaming
+  const url = "http://127.0.0.1:8000/conversation-audio";
   const formData = new FormData();
   formData.append("audio", audioBlob, "audio.wav");
 
@@ -30,55 +34,3 @@ export async function streamAudioFromBackend(audioBlob) {
     throw error;
   }
 }
-
-
-
-/*export async function sendAudioToBackend(audioBlob) {
-  const url = "http://127.0.0.1:8000/voice-to-text";
-  const formData = new FormData();
-  formData.append("audio", audioBlob, "audio.wav");
-
-  try {
-    const response = await fetch(url, { method: "POST", body: formData });
-
-    if (!response.ok) {
-      const errorText = await response.text();
-      throw new Error(
-        `Failed to process audio: ${response.statusText} - ${errorText}`
-      );
-    }
-
-    const data = await response.json();
-    console.log("Received text from backend:", data.text);
-    return data.text;
-  } catch (error) {
-    console.error("Error:", error);
-    alert(`Error: ${error.message}`);
-    throw error;
-  }
-}
-
-export async function playAudioMessage(text) {
-  const url = "http://127.0.0.1:8000/text-to-speech";
-  const response = await fetch(url, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ text }),
-  });
-
-  if (!response.ok) throw new Error("Failed to fetch audio response");
-
-  const blob = await response.blob();
-  const audioUrl = URL.createObjectURL(blob);
-  console.log("Audio URL:", audioUrl);
-
-  //automatically play the audio
-  //uncomment this part to automatically play the audio
-  const audio = new Audio(audioUrl);
-  audio.play().catch(error => console.error("Audio playback error:", error));
-
-
-  //no need for this as audio is currently playing automatically
-  //return audioUrl;
-}
-  */
