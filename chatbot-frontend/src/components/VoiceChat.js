2a6f9a66224b0bac1198073a17406214dce3996b
SarahBeH <sarrabenhamdene01@gmail.com>
2025-01-15 22:18:23 +0100

    adjusted volume Threshold

--- chatbot-frontend/src/components/VoiceChat.js
+++ chatbot-frontend/src/components/VoiceChat.js
@@ -51,7 +51,7 @@ const VoiceChat = ({ onSelectOption, sendAudioToBackend, setChatHistory }) => {
         audioContextRef.current = audioContext;
 
         const dataArray = new Uint8Array(analyser.frequencyBinCount);
-        const volumeThreshold = 70; // Adjust this threshold as needed
+        const volumeThreshold = 50; // Adjust this threshold as needed
 
         const detectSilence = () => {
           analyser.getByteFrequencyData(dataArray);
